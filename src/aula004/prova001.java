c3362f9190290cb17ae60ae7506e2dc927707baf
warleymts <warlinpb@outlook.com>
2024-02-15 20:35:20 -0300

    strings-arrays

---
+++ src/aula004/prova001.java
@@ -0,0 +1,15 @@
+package aula004;
+
+public class prova001 {
+    public static void main(String[] args) {
+
+
+        String[] objetos = {"garrafa", "copo", "litro"};
+
+        for (int tam = objetos.length - 1; tam >= 0; tam--) {
+
+            System.out.print(objetos[tam] + " ");
+
+        }
+    }
+}
