60b028d2c958362be87187e13f97c36f74afdf67
warleymts <warlinpb@outlook.com>
2024-02-15 21:38:55 -0300

    heranca-polimorfirsmo

---
+++ src/aula005/Coordenador.java
@@ -0,0 +1,5 @@
+package aula005;
+
+public interface Coordenador {
+    double calcularSalario();
+}
---
+++ src/aula005/Suporte.java
@@ -0,0 +1,5 @@
+package aula005;
+
+public interface Suporte {
+    double calcularSalario();
+}
